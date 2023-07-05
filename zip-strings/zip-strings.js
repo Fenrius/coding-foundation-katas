d6dfbc6ba145e922a91f4140a5ab7611fc70a95a
Fenrius <jahnke.kristof2@gmail.com>
2023-07-05 15:15:07 +0200

    solution suggestion

--- zip-strings/zip-strings.js
+++ zip-strings/zip-strings.js
@@ -1,3 +1,21 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
+  let mergeStr = "";
+  if (strB.length === 0) {
+    mergeStr += strA;
+    return mergeStr;
+  } else if (strA.length === 0) {
+    mergeStr += strB;
+    return mergeStr;
+  } else {
+    for (let i = 0; i < strA.length + strB.length; i++) {
+      if (i < strA.length) {
+        mergeStr += strA[i];
+      }
+      if (i < strB.length) {
+        mergeStr += strB[i];
+      }
+    }
+    console.log(mergeStr);
+    return mergeStr;
+  }
 }
