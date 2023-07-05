2da6bf17c5659eb2be55d04e9a158240695501ad
Fenrius <jahnke.kristof2@gmail.com>
2023-07-05 14:11:10 +0200

    solution suggestion

--- filter-api-data/filter-api-data.js
+++ filter-api-data/filter-api-data.js
@@ -1,3 +1,16 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  let filteredArr = [];
+  apiData.forEach((entry) => {
+    let hitCount = 0;
+    for (const key in entry) {
+      if (mandatoryKeys.includes(key)) {
+        hitCount++;
+      }
+    }
+    if (hitCount === mandatoryKeys.length) {
+      filteredArr.push(entry);
+    }
+  });
+  return filteredArr;
+  // Array als Endlösung
 }
