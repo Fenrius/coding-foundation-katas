b542865aceddca0399506411c6bfd984d71316c9
Fenrius <jahnke.kristof2@gmail.com>
2023-07-05 11:58:01 +0200

    solution suggestion

--- extract-class-name/extract-class-name.js
+++ extract-class-name/extract-class-name.js
@@ -1,3 +1,13 @@
 function extractClassName(sessionTitle) {
-  // Add you solution here
+  if (sessionTitle.includes("September")) {
+    return "2022-09";
+  } else if (sessionTitle.includes("Januar")) {
+    return "2023-01";
+  } else if (sessionTitle.includes("Maerz")) {
+    return "2022-03";
+  } else if (sessionTitle.includes("März")) {
+    return "2022-03";
+  } else {
+    return null;
+  }
 }
